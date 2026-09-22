2be1ed2c95920b33b68b0f5cd563e00671efcf5e
GiovanniM27 <sonicgio27@gmail.com>
2026-09-22 03:41:05 -0400

    Add Conceptual API interfaces and prototype implementation

---
+++ src/apis/ComputationInput.java
@@ -0,0 +1,14 @@
+package apis;
+
+public class ComputationInput {
+
+    private final int value;
+
+    public ComputationInput(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
---
+++ src/apis/ComputationResult.java
@@ -0,0 +1,14 @@
+package apis;
+
+public class ComputationResult {
+
+    private final String formattedResult;
+
+    public ComputationResult(String formattedResult) {
+        this.formattedResult = formattedResult;
+    }
+
+    public String getFormattedResult() {
+        return formattedResult;
+    }
+}
